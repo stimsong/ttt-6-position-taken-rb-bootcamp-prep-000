a99eecf9be991c8126ce37adce0b235a6ac37848
Greg Stimson <stimsong@yahoo.com>
2018-02-04 20:20:51 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if board[index] == " " or "" or nil
-    return false
-  elsif board[index] == "X" or "O"
+  if board[index] == "X" or "O"
     return true
+  elsif board[index] == " " or "" or nil
+    return false
   end
 end
