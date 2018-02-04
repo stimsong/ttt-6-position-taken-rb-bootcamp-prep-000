5559db9b4958d83b9cdffe1d919b00c6937020ee
Greg Stimson <stimsong@yahoo.com>
2018-02-04 20:28:07 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 def position_taken?(board, index)
   if board[index] == "X" or "O"
     true
-  elsif board[index] == " " or "" or nil
+  elsif board[index] != " " or "" or nil
     false
   end
 end
